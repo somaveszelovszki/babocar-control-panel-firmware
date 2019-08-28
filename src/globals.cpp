2f1868ef9bc5a3dc2125d3cf909302cc0453223c
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2019-08-28 18:38:33 +0200

    global parameters enabled

--- src/globals.cpp
+++ src/globals.cpp
@@ -12,21 +12,18 @@ bool useSafetyEnableSignal = true;
 bool indicatorLedsEnabled  = true;
 bool startSignalEnabled    = false;
 bool lineFollowEnabled     = true;
-bool targetSpeedOverride   = false;
 
 m_per_sec_t      targetSpeed(0.0f);
 atomic<CarProps> car(cfg::mutex_Car);
 
 void initializeGlobalParams() {
     REGISTER_GLOBAL(useSafetyEnableSignal);
-//    REGISTER_GLOBAL(indicatorLedsEnabled);
-//    REGISTER_GLOBAL(startSignalEnabled);
-//    REGISTER_GLOBAL(lineFollowEnabled);
-//    REGISTER_GLOBAL(targetSpeedOverride);
-//
-//    REGISTER_GLOBAL(logLevel_t);
-//    REGISTER_GLOBAL(car);
-//    REGISTER_GLOBAL(targetSpeed);
+    REGISTER_GLOBAL(indicatorLedsEnabled);
+    REGISTER_GLOBAL(startSignalEnabled);
+    REGISTER_GLOBAL(lineFollowEnabled);
+
+    REGISTER_GLOBAL(car);
+    REGISTER_GLOBAL(targetSpeed);
 }
 
 }  // namespace globals
