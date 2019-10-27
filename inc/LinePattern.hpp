eee64f2d253bb07663d50d9f92614eb8bac75931
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2019-10-27 21:50:04 +0100

    Line pattern state machine added for race track patterns

--- inc/LinePattern.hpp
+++ inc/LinePattern.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <micro/utils/units.hpp>
+#include <micro/utils/Line.hpp>
 #include <micro/utils/point2.hpp>
 
 namespace micro {
@@ -9,11 +9,9 @@ namespace micro {
  **/
 struct LinePattern {
 public:
-    static const point2<meter_t> UNKNOWN_POS;
-
     /* @brief Defines line pattern type.
      **/
-    enum class Type : uint32_t {
+    enum Type {
         SINGLE_LINE = 0,    // 1 solid 1.9mm wide line.
         ACCELERATE  = 1,    // 1 solid line in the middle, 2 dashed lines on both sides
         BRAKE       = 2,    // 1 solid line in the middle, 2 solid lines on both sides
@@ -21,18 +19,54 @@ public:
         JUNCTION    = 4     // Labyrinth junction
     };
 
-    Type type;          // Line pattern type.
-    Sign dir;           // The pattern direction (POSITIVE means it is the same as the car direction). e.g. at a junction: POSITIVE means the car has two options to choose from (forward or exit)
+    Type type;        // Line pattern type.
+    Sign dir;         // The pattern direction (POSITIVE means it is the same as the car direction).
     Direction side;   // The side of the line the pattern is on. In case of symmetrical pattern: CENTER.
-    point2<meter_t> startPos;   // The pattern start position.
-    point2<meter_t> endPos;     // The pattern end position.
+    meter_t startDist;
 
-    LinePattern()
+    LinePattern(void)
         : type(Type::SINGLE_LINE)
         , dir(Sign::POSITIVE)
         , side(Direction::CENTER)
-        , startPos(UNKNOWN_POS)
-        , endPos(UNKNOWN_POS) {}
+        , startDist(0) {}
+
+    LinePattern(Type type, Sign dir, Direction side, meter_t startDist)
+        : type(type)
+        , dir(dir)
+        , side(side)
+        , startDist(startDist) {}
+};
+
+class LinePatternCalculator {
+public:
+    LinePatternCalculator(void)
+        : currentMeasIdx(0)
+        , isCurrentPatternValidated(true)
+        , isPatternChangeCheckNeeded(false) {}
+
+    void update(meter_t currentDist, const Lines& lines);
+private:
+    struct StampedLines {
+        meter_t distance;
+        Lines lines;
+    };
+
+    void changePattern(LinePattern newPattern);
+
+    meter_t distanceSinceNumLinesIs(uint8_t numLines, meter_t currentDist) const;
+
+    bool isPatternValid(LinePattern::Type patternType, const Lines& lines, meter_t currentDist);
+
+    static constexpr uint8_t PREV_MEAS_SIZE = 100;
+
+    StampedLines prevMeas[PREV_MEAS_SIZE];
+    uint8_t currentMeasIdx;
+
+    LinePattern prevPrevPattern;
+    LinePattern prevPattern;
+    LinePattern currentPattern;
+    bool isCurrentPatternValidated;
+    bool isPatternChangeCheckNeeded;
 };
 
 } // namespace micro
--- src/LinePattern.cpp
+++ src/LinePattern.cpp
@@ -1,7 +1,160 @@
 #include <LinePattern.hpp>
+#include <globals.hpp>
 
 namespace micro {
 
-const point2<meter_t> LinePattern::UNKNOWN_POS = { meter_t(100), meter_t(100) };
+static constexpr meter_t ACCELERATE_SECTION_LENGTH   = centimeter_t(8);
+static constexpr meter_t ACCELERATE_SPACE_LENGTH     = centimeter_t(8);
+static constexpr meter_t ACCELERATE_VALIDITY_LENGTH  = centimeter_t(16);
+static constexpr meter_t ACCELERATE_PATTERN_LENGTH   = centimeter_t(72);
+
+static constexpr meter_t VALIDITY_LENGTH_SINGLE_LINE = centimeter_t(20);
+
+static constexpr meter_t BRAKE_VALIDITY_LENGTH       = centimeter_t(20);
+static constexpr meter_t BRAKE_PATTERN_LENGTH        = centimeter_t(300);
+
+void LinePatternCalculator::update(meter_t currentDist, const Lines& lines) {
+    this->prevMeas[currentMeasIdx] = { currentDist, lines };
+
+    switch(this->currentPattern.type) {
+
+    case LinePattern::SINGLE_LINE:
+    {
+        switch(globals::programState.activeModule()) {
+
+        case ProgramState::ActiveModule::Labyrinth:
+        {
+            break;
+        }
+
+        case ProgramState::ActiveModule::RaceTrack:
+        {
+            if (lines.size() == 3) {
+                this->isPatternChangeCheckNeeded;
+            }
+
+            if (this->isPatternChangeCheckNeeded) {
+                if (this->isPatternValid(LinePattern::ACCELERATE, lines, currentDist)) {
+                    this->changePattern({ LinePattern::ACCELERATE, Sign::POSITIVE, Direction::CENTER, currentDist });
+                } else if (this->isPatternValid(LinePattern::BRAKE, lines, currentDist)) {
+                    this->changePattern({ LinePattern::BRAKE, Sign::POSITIVE, Direction::CENTER, currentDist });
+                }
+            }
+            break;
+        }
+
+        default:
+            break;
+        }
+
+        break;
+    }
+
+    case LinePattern::ACCELERATE:
+    {
+        if (this->isCurrentPatternValidated) {
+            if (abs(currentDist - this->currentPattern.startDist) >= ACCELERATE_PATTERN_LENGTH + centimeter_t(5)) {
+                this->changePattern({ LinePattern::SINGLE_LINE, Sign::POSITIVE, Direction::CENTER, currentDist });
+            }
+        } else if (this->isPatternValid(LinePattern::ACCELERATE, lines, currentDist)) {
+            if (abs(currentDist - this->currentPattern.startDist) >= ACCELERATE_VALIDITY_LENGTH) {
+                this->isCurrentPatternValidated = true;
+            }
+        } else {
+            this->changePattern({ LinePattern::SINGLE_LINE, Sign::POSITIVE, Direction::CENTER, currentDist });
+        }
+        break;
+    }
+
+    case LinePattern::BRAKE:
+    {
+        if (this->isCurrentPatternValidated) {
+            if (abs(currentDist - this->currentPattern.startDist) >= BRAKE_PATTERN_LENGTH + centimeter_t(5)) {
+                this->changePattern({ LinePattern::SINGLE_LINE, Sign::POSITIVE, Direction::CENTER, currentDist });
+            }
+        } else if (!this->isPatternValid(LinePattern::BRAKE, lines, currentDist)) {
+            this->changePattern({ LinePattern::SINGLE_LINE, Sign::POSITIVE, Direction::CENTER, currentDist });
+        } else if (abs(currentDist - this->currentPattern.startDist) >= BRAKE_VALIDITY_LENGTH) {
+            this->isCurrentPatternValidated = true;
+        }
+        break;
+    }
+
+    case LinePattern::LANE_CHANGE:
+    {
+        break;
+    }
+
+    case LinePattern::JUNCTION:
+    {
+        break;
+    }
+
+    default:
+        break;
+    }
+
+    currentMeasIdx = (currentMeasIdx + 1) % PREV_MEAS_SIZE;
+}
+
+void LinePatternCalculator::changePattern(LinePattern newPattern) {
+    if (this->isCurrentPatternValidated) {
+        this->prevPrevPattern = this->prevPattern;
+        this->prevPattern = this->currentPattern;
+    }
+    this->currentPattern = newPattern;
+    this->isCurrentPatternValidated = false;
+    this->isPatternChangeCheckNeeded = false;
+}
+
+meter_t LinePatternCalculator::distanceSinceNumLinesIs(uint8_t numLines, meter_t currentDist) const {
+    meter_t dist = meter_t::ZERO();
+    uint8_t i = this->currentMeasIdx;
+
+    do {
+        const StampedLines& l = this->prevMeas[i];
+        if (l.lines.size() != numLines) {
+            break;
+        }
+        dist = currentDist - l.distance;
+
+    } while ((i = i > 0 ? i - 1 : PREV_MEAS_SIZE - 1) != this->currentMeasIdx);
+
+    return dist;
+}
+
+bool LinePatternCalculator::isPatternValid(LinePattern::Type patternType, const Lines& lines, meter_t currentDist) {
+    bool valid = false;
+
+    switch (patternType) {
+    case LinePattern::SINGLE_LINE:
+        valid = 1 == lines.size();
+        break;
+
+    case LinePattern::ACCELERATE:
+        valid = (3 == lines.size() && this->distanceSinceNumLinesIs(3, currentDist) < ACCELERATE_SECTION_LENGTH + centimeter_t(4)) ||
+                (1 == lines.size() && this->distanceSinceNumLinesIs(1, currentDist) < ACCELERATE_SPACE_LENGTH + centimeter_t(4));
+        break;
+
+    case LinePattern::BRAKE:
+        valid = 3 == lines.size();
+        break;
+
+    case LinePattern::LANE_CHANGE:
+        valid =false;
+        break;
+
+    case LinePattern::JUNCTION:
+        valid =false;
+        break;
+
+    default:
+        valid = false;
+        break;
+    }
+
+    return valid;
+}
+
 
 } // namespace micro
