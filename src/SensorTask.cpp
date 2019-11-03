bfeb137afa864fd108ac550c607d4127103c5492
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2019-11-03 21:06:34 +0100

    Distance sensor driver moved to micro-utils, wrapper class implemented

--- src/SensorTask.cpp
+++ src/SensorTask.cpp
@@ -6,10 +6,9 @@
 #include <micro/panel/MotorPanel.hpp>
 #include <micro/panel/MotorPanelData.h>
 #include <micro/hw/MPU9250_Gyroscope.hpp>
+#include <micro/hw/VL53L1X_DistanceSensor.hpp>
 #include <micro/task/common.hpp>
 #include <micro/sensor/Filter.hpp>
-
-#include "VL53L1X_api.h"
 
 #include <DetectedLines.hpp>
 #include <cfg_car.hpp>
@@ -29,6 +28,7 @@ LineDetectPanel rearLineDetectPanel(uart_RearLineDetectPanel);
 LinePatternCalculator linePatternCalc;
 
 hw::MPU9250 gyro(i2c_Gyro, hw::Ascale::AFS_2G, hw::Gscale::GFS_250DPS, hw::Mscale::MFS_16BITS, MMODE_ODR_100Hz);
+hw::VL53L1X_DistanceSensor frontDistSensor(i2c_Dist, 0x52);
 
 Timer lineDetectPanelsSendTimer;
 
@@ -51,64 +51,18 @@ extern "C" void runSensorTask(const void *argument) {
 
     vTaskDelay(5); // gives time to other tasks to initialize their queues
 
-    uint16_t dev=0x52;
-    int status=0;
-    uint8_t byteData, sensorState=0;
-    uint16_t wordData;
-    uint8_t ToFSensor = 1; // 0=Left, 1=Center(default), 2=Right
-    uint16_t Distance;
-    uint16_t SignalRate;
-    uint16_t AmbientRate;
-    uint16_t SpadNum;
-    uint8_t RangeStatus;
-    uint8_t dataReady;
+    frontDistSensor.initialize();
 
-    status = VL53L1_RdByte(dev, 0x010F, &byteData);
-    LOG_DEBUG("VL53L1X Model_ID: %X\n", byteData);
-    status = VL53L1_RdByte(dev, 0x0110, &byteData);
-    LOG_DEBUG("VL53L1X Module_Type: %X\n", byteData);
-    status = VL53L1_RdWord(dev, 0x010F, &wordData);
-    LOG_DEBUG("VL53L1X: %X\n", wordData);
-
-    while(sensorState == 0){
-        status = VL53L1X_BootState(dev, &sensorState);
-        vTaskDelay(2);
-    }
-    LOG_DEBUG("Chip booted\n");
-
-    /* This function must to be called to initialize the sensor with the default setting  */
-    status = VL53L1X_SensorInit(dev);
-    /* Optional functions to be used to change the main ranging parameters according the application requirements to get the best ranging performances */
-    status = VL53L1X_SetDistanceMode(dev, 1); /* 1=short, 2=long */
-    status = VL53L1X_SetTimingBudgetInMs(dev, 15); /* in ms possible values [20, 50, 100, 200, 500] */
-    status = VL53L1X_SetInterMeasurementInMs(dev, 15); /* in ms, IM must be > = TB */
-    //  status = VL53L1X_SetOffset(dev,20); /* offset compensation in mm */
-    //  status = VL53L1X_SetROI(dev, 16, 16); /* minimum ROI 4,4 */
-    //  status = VL53L1X_CalibrateOffset(dev, 140, &offset); /* may take few second to perform the offset cal*/
-    //  status = VL53L1X_CalibrateXtalk(dev, 1000, &xtalk); /* may take few second to perform the xtalk cal */
-    LOG_DEBUG("VL53L1X Ultra Lite Driver Example running ...\n");
-    status = VL53L1X_StartRanging(dev);   /* This function has to be called to enable the ranging */
     millisecond_t prevDistReadTime;
-    while(1){ /* read and display data */
-        while (dataReady == 0){
-            status = VL53L1X_CheckForDataReady(dev, &dataReady);
-            vTaskDelay(2);
+    while(1) {
+
+        meter_t distance;
+
+        if (isOk(frontDistSensor.readDistance(distance))) {
+            LOG_DEBUG("%fms: %u", (getTime() - prevDistReadTime).get(), static_cast<uint32_t>(static_cast<millimeter_t>(distance).get()));
+            prevDistReadTime = getTime();
         }
-        dataReady = 0;
-        status = VL53L1X_GetRangeStatus(dev, &RangeStatus);
-        status = VL53L1X_GetDistance(dev, &Distance);
-        status = VL53L1X_GetSignalRate(dev, &SignalRate);
-        status = VL53L1X_GetAmbientRate(dev, &AmbientRate);
-        status = VL53L1X_GetSpadNb(dev, &SpadNum);
-        status = VL53L1X_ClearInterrupt(dev); /* clear interrupt has to be called to enable next interrupt*/
-        LOG_DEBUG("%fms: %u, %u, %u, %u, %u",
-            (getTime() - prevDistReadTime).get(), (uint32_t)RangeStatus, (uint32_t)Distance, (uint32_t)SignalRate, (uint32_t)AmbientRate, (uint32_t)SpadNum);
-
-        prevDistReadTime = getTime();
-    }
-
-    while(true) {
-        vTaskDelay(50);
+        vTaskDelay(10);
     }
 
 //    Lines lines;
