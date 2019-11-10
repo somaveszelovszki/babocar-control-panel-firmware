8fdb82ff87f86a7c37c83665d4ae6f2086e8d789
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2019-11-10 22:09:56 +0100

    communication with front line detector panel tested

--- src/SensorTask.cpp
+++ src/SensorTask.cpp
@@ -53,53 +53,63 @@ extern "C" void runSensorTask(const void *argument) {
 
     frontDistSensor.initialize();
 
-    millisecond_t prevDistReadTime;
-    while(1) {
+    Lines lines;
+    Line mainLine;
 
-        meter_t distance;
+    LowPassFilter<degree_t, 5> gyroAngleFilter;
 
-        if (isOk(frontDistSensor.readDistance(distance))) {
-            LOG_DEBUG("%fms: %u", (getTime() - prevDistReadTime).get(), static_cast<uint32_t>(static_cast<millimeter_t>(distance).get()));
-            prevDistReadTime = getTime();
-        }
-        vTaskDelay(10);
-    }
+    lineDetectPanelDataIn_t frontLineDetectPanelData;
+    fillLineDetectPanelData(frontLineDetectPanelData);
+    frontLineDetectPanel.start(frontLineDetectPanelData);
 
-//    Lines lines;
-//    Line mainLine;
-//
-//    LowPassFilter<degree_t, 5> gyroAngleFilter;
-//
-//    lineDetectPanelDataIn_t frontLineDetectPanelData;
-//    fillLineDetectPanelData(frontLineDetectPanelData);
-//    frontLineDetectPanel.start(frontLineDetectPanelData);
-//
 //    lineDetectPanelDataIn_t rearLineDetectPanelData;
 //    fillLineDetectPanelData(rearLineDetectPanelData);
 //    rearLineDetectPanel.start(rearLineDetectPanelData);
-//
-//    gyro.initialize();
-//
-//    frontLineDetectPanel.waitStart();
-//    rearLineDetectPanel.waitStart();
-//    lineDetectPanelsSendTimer.start(millisecond_t(100));
-//
-//    while (true) {
-//
-//        const point3<gauss_t> mag = gyro.readMagData();
-//        if (!isZero(mag.X) || !isZero(mag.Y) || !isZero(mag.Z)) {
-//            globals::car.pose.angle = normalize360(gyroAngleFilter.update(atan2(mag.Y, mag.X)));
-//            LOG_DEBUG("orientation: %f deg", static_cast<degree_t>(globals::car.pose.angle).get());
-//        }
-//
-//        if (lineDetectPanelsSendTimer.checkTimeout()) {
-//            fillLineDetectPanelData(frontLineDetectPanelData);
-//            frontLineDetectPanel.send(frontLineDetectPanelData);
-//
-//            fillLineDetectPanelData(rearLineDetectPanelData);
-//            rearLineDetectPanel.send(rearLineDetectPanelData);
-//        }
-//
+
+    gyro.initialize();
+
+    frontLineDetectPanel.waitStart();
+    //rearLineDetectPanel.waitStart();
+    lineDetectPanelsSendTimer.start(millisecond_t(100));
+
+    while (true) {
+
+        const point3<gauss_t> mag = gyro.readMagData();
+        if (!isZero(mag.X) || !isZero(mag.Y) || !isZero(mag.Z)) {
+            globals::car.pose.angle = normalize360(gyroAngleFilter.update(atan2(mag.Y, mag.X)));
+            //LOG_DEBUG("orientation: %f deg", static_cast<degree_t>(globals::car.pose.angle).get());
+        }
+
+        meter_t distance;
+        if (isOk(frontDistSensor.readDistance(distance))) {
+            //LOG_DEBUG("front distance: %u mm", static_cast<uint32_t>(static_cast<millimeter_t>(distance).get()));
+        }
+
+        if (lineDetectPanelsSendTimer.checkTimeout()) {
+            fillLineDetectPanelData(frontLineDetectPanelData);
+            frontLineDetectPanel.send(frontLineDetectPanelData);
+
+            //fillLineDetectPanelData(rearLineDetectPanelData);
+            //rearLineDetectPanel.send(rearLineDetectPanelData);
+        }
+
+        if (frontLineDetectPanel.hasNewValue()) {
+            LinePositions frontLinePositions, rearLinePositions;
+
+            getLinesFromPanel(frontLineDetectPanel, frontLinePositions);
+            //getLinesFromPanel(rearLineDetectPanel, rearLinePositions);
+
+            //calculateLines(frontLinePositions, rearLinePositions, lines, mainLine);
+
+            mainLine.pos_front = frontLinePositions.size() > 1 ? frontLinePositions[1] : frontLinePositions[0];
+
+            LOG_DEBUG("front line: %d mm", static_cast<int32_t>(static_cast<millimeter_t>(mainLine.pos_front).get()));
+
+            linePatternCalc.update(globals::car.distance, frontLinePositions, rearLinePositions, lines);
+            const DetectedLines detectedLines = { lines, mainLine, linePatternCalc.getPattern() };
+            xQueueOverwrite(detectedLinesQueue, &detectedLines);
+        }
+
 //        if (frontLineDetectPanel.hasNewValue() && rearLineDetectPanel.hasNewValue()) {
 //            LinePositions frontLinePositions, rearLinePositions;
 //
@@ -111,9 +121,9 @@ extern "C" void runSensorTask(const void *argument) {
 //            const DetectedLines detectedLines = { lines, mainLine, linePatternCalc.getPattern() };
 //            xQueueOverwrite(detectedLinesQueue, &detectedLines);
 //        }
-//
-//        vTaskDelay(1);
-//    }
+
+        vTaskDelay(1);
+    }
 
     vTaskDelete(nullptr);
 }
