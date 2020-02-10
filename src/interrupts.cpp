9402a7b5182698f14283f92f06b1c37df9bc5056
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2020-02-10 22:23:11 +0100

    unused interrupt handlers removed

--- src/interrupts.cpp
+++ src/interrupts.cpp
@@ -19,34 +19,6 @@ extern void micro_RadioModule_Uart_RxCpltCallback();                        // C
 extern void micro_FrontLineDetectPanel_Uart_RxCpltCallback();               // Callback for front line detect panel UART RxCplt - called when receive finishes.
 extern void micro_RearLineDetectPanel_Uart_RxCpltCallback();                // Callback for rear line detect panel UART RxCplt - called when receive finishes.
 
-/* @brief Internal callback - called when SPI reception finishes.
- * @param hspi Pointer to the SPI handle.
- **/
-extern "C" void HAL_SPI_RxCpltCallback(SPI_HandleTypeDef *hspi) {
-
-}
-
-/* @brief Internal callback - called when SPI transmission finishes.
- * @param hspi Pointer to the SPI handle.
- **/
-extern "C" void HAL_SPI_TxCpltCallback(SPI_HandleTypeDef *hspi) {
-
-}
-
-/* @brief Internal callback - called when SPI exchange finishes.
- * @param hspi Pointer to the SPI handle.
- **/
-extern "C" void HAL_SPI_TxRxCpltCallback(SPI_HandleTypeDef *hspi) {
-
-}
-
-/* @brief Internal callback - called when I2C reception finishes.
- * @param hi2c Pointer to the I2C handle.
- **/
-extern "C" void HAL_I2C_SlaveRxCpltCallback(I2C_HandleTypeDef *hi2c) {
-
-}
-
 /* @brief Internal callback - called when UAR receive finishes.
  * @param huart Pointer to the UART handle.
  **/
@@ -75,15 +47,9 @@ extern "C" void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart) {
     }
 }
 
-extern "C" void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart) {}
-
 /* @brief Callback function for timer period elapsed event.
  * @param htim Pointer to the timer handle.
  **/
 extern "C" void micro_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
     micro::Timer::updateAll();
 }
-
-extern "C" void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim) {}
-
-extern "C" void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {}
