0ec988d05cffbc75ca254d12a6ab92c51246343b
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2020-11-01 23:23:36 +0100

    CAR_FRONT_REAR_SENSOR_ROW_DIST set

--- include/cfg_car.hpp
+++ include/cfg_car.hpp
@@ -6,7 +6,7 @@ namespace cfg {
 
 constexpr micro::meter_t  CAR_FRONT_REAR_PIVOT_DIST       = micro::millimeter_t(269.5f);
 constexpr micro::meter_t  CAR_PIVOT_LENGTH                = micro::millimeter_t(263);
-constexpr micro::meter_t  CAR_FRONT_REAR_SENSOR_ROW_DIST  = micro::millimeter_t(0);
+constexpr micro::meter_t  CAR_FRONT_REAR_SENSOR_ROW_DIST  = micro::millimeter_t(447);
 constexpr micro::meter_t  OPTO_SENSOR_FRONT_WHEEL_DIST    = micro::millimeter_t(107.0f);
 
 constexpr micro::radian_t WHEEL_MAX_DELTA                 = micro::degree_t(20);
