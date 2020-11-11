7e2f4e7865ab2b765a5998ebe4241d2023dbbf47
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2020-11-11 20:22:25 +0100

    Wheel max delta increased to 24 degrees

--- include/cfg_car.hpp
+++ include/cfg_car.hpp
@@ -9,7 +9,7 @@ constexpr micro::meter_t  CAR_PIVOT_LENGTH                = micro::millimeter_t(
 constexpr micro::meter_t  CAR_FRONT_REAR_SENSOR_ROW_DIST  = micro::millimeter_t(447);
 constexpr micro::meter_t  OPTO_SENSOR_FRONT_WHEEL_DIST    = micro::millimeter_t(107.0f);
 
-constexpr micro::radian_t WHEEL_MAX_DELTA                 = micro::degree_t(20);
+constexpr micro::radian_t WHEEL_MAX_DELTA                 = micro::degree_t(24);
 constexpr micro::radian_t DIST_SENSOR_SERVO_MAX_DELTA     = micro::degree_t(45);
 constexpr float           DIST_SENSOR_SERVO_TRANSFER_RATE = 1.0f;
 constexpr bool            DIST_SENSOR_SERVO_ENABLED       = false;
