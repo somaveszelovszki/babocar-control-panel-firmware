79973a8e24fe00b63dd80fb4f2e594fbb37cd28c
Bence Sebők <sebok.bence.003@gmail.com>
2021-02-04 15:30:43 +0100

    Add basic tests from U segment

--- test/src/utest_race_labyrinth.cpp
+++ test/src/utest_race_labyrinth.cpp
@@ -13,10 +13,25 @@ TEST(race_labyrinth, valid) {
     ASSERT_TRUE(graph.valid());
 }
 
-TEST(race_labyrinth, W_J) {
+TEST(race_labyrinth, U_T) {
     LabyrinthGraph graph = buildRaceLabyrinthGraph();
 
-    const Segment& src         = *graph.findSegment('W');
+    const Segment& src         = *graph.findSegment('U');
+    const Segment& dest        = *graph.findSegment('T');
+    const Junction& prevJunc   = *graph.findJunction(13);
+    const Connection& prevConn = **std::find_if(src.edges.begin(), src.edges.end(), [&prevJunc](const Connection *c) {
+        return c->junction == &prevJunc;
+    });
+
+    checkRoute(prevConn, src, dest, false, {
+        { graph.findJunction(12), JunctionDecision(radian_t(PI_2), Direction::LEFT) }
+    });
+}
+
+TEST(race_labyrinth, U_J) {
+    LabyrinthGraph graph = buildRaceLabyrinthGraph();
+
+    const Segment& src         = *graph.findSegment('U');
     const Segment& dest        = *graph.findSegment('J');
     const Junction& prevJunc   = *graph.findJunction(13);
     const Connection& prevConn = **std::find_if(src.edges.begin(), src.edges.end(), [&prevJunc](const Connection *c) {
@@ -24,22 +39,6 @@ TEST(race_labyrinth, W_J) {
     });
 
     checkRoute(prevConn, src, dest, false, {
-        { graph.findJunction(12), JunctionDecision(radian_t(PI), Direction::RIGHT) }
-    });
-}
-
-TEST(race_labyrinth, W_M) {
-    LabyrinthGraph graph = buildRaceLabyrinthGraph();
-
-    const Segment& src         = *graph.findSegment('W');
-    const Segment& dest        = *graph.findSegment('M');
-    const Junction& prevJunc   = *graph.findJunction(13);
-    const Connection& prevConn = **std::find_if(src.edges.begin(), src.edges.end(), [&prevJunc](const Connection *c) {
-        return c->junction == &prevJunc;
-    });
-
-    checkRoute(prevConn, src, dest, false, {
-        { graph.findJunction(12), JunctionDecision(radian_t(PI), Direction::LEFT) },
-        { graph.findJunction(10), JunctionDecision(radian_t(3 * PI_2), Direction::RIGHT) }
+        { graph.findJunction(12), JunctionDecision(radian_t(PI_2), Direction::RIGHT) }
     });
 }
