cff848e10337c41bf5bbc552f541446104ee6131
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2021-02-17 22:52:21 +0100

    low-speed line control parameters updated for smoother labyrinth cruising

--- src/platform/ControlTask.cpp
+++ src/platform/ControlTask.cpp
@@ -38,8 +38,8 @@ struct ServoOffsets {
 sorted_map<m_per_sec_t, PID_Params, 20> frontLinePosControllerParams = {
     // speed        P      I      D
     { { 0.00f }, { 0.00f, 0.00f,   0.00f } },
-    { { 0.10f }, { 2.40f, 0.00f, 120.00f } },
-    { { 1.00f }, { 2.40f, 0.00f, 120.00f } },
+    { { 0.10f }, { 2.20f, 0.00f, 120.00f } },
+    { { 1.00f }, { 2.20f, 0.00f, 120.00f } },
     { { 1.50f }, { 1.80f, 0.00f, 120.00f } },
     { { 2.00f }, { 1.70f, 0.00f, 120.00f } },
     { { 2.25f }, { 1.70f, 0.00f, 120.00f } },
@@ -55,8 +55,8 @@ sorted_map<m_per_sec_t, PID_Params, 20> frontLinePosControllerParams = {
 sorted_map<m_per_sec_t, PID_Params, 20> rearLineAngleControllerParams = {
     // speed        P      I      D
     { { 0.00f }, { 0.00f, 0.00f, 0.00f  } },
-    { { 0.10f }, { 0.80f, 0.00f, 30.00f } },
-    { { 1.00f }, { 0.80f, 0.00f, 30.00f } },
+    { { 0.10f }, { 0.60f, 0.00f, 30.00f } },
+    { { 1.00f }, { 0.60f, 0.00f, 30.00f } },
     { { 1.50f }, { 0.50f, 0.00f, 30.00f } },
     { { 2.00f }, { 0.40f, 0.00f, 40.00f } },
     { { 2.25f }, { 0.40f, 0.00f, 40.00f } },
